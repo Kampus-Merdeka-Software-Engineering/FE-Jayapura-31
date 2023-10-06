8bcba92143bd72c7970f63b16bc0de8c3e40ec15
HALCYON9 <faqih.alatas12@gmail.com>
2023-10-06 20:08:50 +0700

    js fix

--- Pages/js/beranda.js
+++ Pages/js/beranda.js
@@ -4,3 +4,20 @@ const mobileMenu = document.querySelector('.mobile-menu');
 menuToggle.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
+
+document.getElementById("testimoniForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = document.getElementById("email");
+  const userEmail = emailInput.value;
+
+  
+  const registeredEmails = ["user1@example.com", "user2@example.com", "user3@example.com"];
+
+  if (registeredEmails.includes(userEmail)) {
+      alert("Testimoni berhasil dikirim.");
+      
+  } else {
+      alert("Alamat email tidak terdaftar. Anda tidak dapat mengirimkan testimoni.");
+  }
+});
--- Pages/js/booking-page.js
+++ Pages/js/booking-page.js
@@ -4,3 +4,28 @@ const mobileMenu = document.querySelector('.mobile-menu');
 menuToggle.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
+
+const urlParams = new URLSearchParams(window.location.search);
+      const selectedOption = urlParams.get("selectedOption");
+
+      if (selectedOption) {
+        const selectElement = document.getElementById("paket-konseling");
+        selectElement.value = selectedOption;
+      }
+      document
+        .getElementById("bookingForm")
+        .addEventListener("submit", function (event) {
+          event.preventDefault(); 
+
+          alert(
+            "Data telah berhasil terkirim untuk lebih lanjut anda akan dihubungi melalui Email. Terima kasih telah mendaftar!"
+          );
+        });
+
+        
+      const form = document.getElementById('bookingForm');
+      form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+      window.location.href = '/summary.html'
+      });
