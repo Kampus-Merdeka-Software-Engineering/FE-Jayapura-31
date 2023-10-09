18723db49ca28967416223351daaa482c9ab1a33
Floriane Cheryl Hendarsah <cherylhendarsah@gmail.com>
2023-10-09 20:55:38 +0700

    js summary

---
+++ Pages/js/summary.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const okeButton = document.getElementById('okeButton');
+    okeButton.addEventListener("click", function () {
+      window.location.href = "/index.html"; 
+    });
+  });
